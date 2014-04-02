06e1f57b68eda8030e267bf12f6b0209db5d3404
Daniel Ostermeier <daniel.ostermeier@gmail.com>
2014-04-02 10:00:30 +0200

    comment and test

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # Vundle bundle manager for vim.
+#
+# To install the individual plugins, run vim > :PluginInstall 
+# The plugins to be downloaded are defined in .vimrc
+#
 vim_home="$HOME/.vim"
 if [ ! -d "$vim_home/bundle/vundle" ]; then
     echo "Downloading Vundle [vim bundle manager] ..."
