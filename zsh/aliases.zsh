8e8711cc3818d28e39a7b2a388c5ab8d8a0d34d2
Daniel Ostermeier <daniel.ostermeier@gmail.com>
2014-09-12 09:42:50 +0200

    removed alias's that were not being used.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,9 +1,3 @@
-PROJECT_HOME=/Users/dostermeier/Code
-
-alias cd-main='cd $PROJECT_HOME/git.booking.com/main' 
-alias cd-security='cd $PROJECT_HOME/git.booking.com/security'
-alias cd-dotfiles='cd $HOME/.dotfiles'
-
 alias who='who -H -u -T'
 
 # Get Aliases
